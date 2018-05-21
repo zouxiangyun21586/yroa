9ccceae53c525d7ccbb226c4adf1ca9f0cc4bd7a
zhouyehaogit2498 <2498230018@qq.com>
2018-05-21 11:05:47 +0800

    加了个注释

--- src/main/java/com/yr/controller/AccountController.java
+++ src/main/java/com/yr/controller/AccountController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.yr.entry.Account;
 import com.yr.service.AccountService;
 
+/**
+ * 账户Controller
+ * @author 周业好
+ * 2018年5月21日 上午10:55:46
+ */
 @Controller // 表示这个是一个Controller
 @RequestMapping("acc")
 public class AccountController {
